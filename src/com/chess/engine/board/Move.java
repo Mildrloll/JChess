6feffb4354954be8011f449b27bc0fcd2726d53f
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-01-06 10:22:28 +0100

    Done with the castle moves in the moves class for now

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -2,6 +2,7 @@ package com.chess.engine.board;
 
 import com.chess.engine.pieces.Pawn;
 import com.chess.engine.pieces.Piece;
+import com.chess.engine.pieces.Rook;
 
 import static com.chess.engine.board.Board.*;
 
@@ -174,23 +175,81 @@ public abstract class Move {
     }
 
     static abstract class CastleMove extends Move {
+        protected final Rook castleRook;
+        protected final int castleRookStart;
+        protected final int castleRookDestination;
 
-        public CastleMove(final Board board, final Piece movedPiece, final int destinationCoordinate) {
+        public CastleMove(final Board board,
+                          final Piece movedPiece,
+                          final int destinationCoordinate,
+                          final Rook castleRook,
+                          final int castleRookStart,
+                          final int castleRookDestination) {
             super(board, movedPiece, destinationCoordinate);
+            this.castleRook = castleRook;
+            this.castleRookStart = castleRookStart;
+            this.castleRookDestination = castleRookDestination;
+        }
+
+        public Rook getCastleRook() {
+            return this.castleRook;
+        }
+
+        @Override
+        public boolean isCastlingMove() {
+            return true;
+        }
+
+        @Override
+        public Board execute() {
+            final Builder builder = new Builder();
+            for (final Piece piece : this.board.currentPlayer().getActivePieces()) {
+                if (!this.movedPiece.equals(piece) && !this.castleRook.equals(piece)) {
+                    builder.setPiece(piece);
+                }
+            }
+            for (final Piece piece : this.board.currentPlayer().getOpponent().getActivePieces()) {
+                builder.setPiece(piece);
+            }
+            builder.setPiece(this.movedPiece.movePiece(this));
+            //TODO look into the first move on normal pieces
+            builder.setPiece(new Rook(this.castleRook.getPieceAlliance(), this.castleRookDestination));
+            builder.setMoveMaker(this.board.currentPlayer().getOpponent().getAlliance());
+            return builder.build();
         }
     }
 
     public static final class KingSideCastleMove extends CastleMove {
 
-        public KingSideCastleMove(final Board board, final Piece movedPiece, final int destinationCoordinate) {
-            super(board, movedPiece, destinationCoordinate);
+        public KingSideCastleMove(final Board board,
+                                  final Piece movedPiece,
+                                  final int destinationCoordinate,
+                                  final Rook castleRook,
+                                  final int castleRookStart,
+                                  final int castleRookDestination) {
+            super(board, movedPiece, destinationCoordinate, castleRook, castleRookStart, castleRookDestination);
+        }
+
+        @Override
+        public String toString() {
+            return "0-0";
         }
     }
 
     public static final class QueenSideCastleMove extends CastleMove {
 
-        public QueenSideCastleMove(final Board board, final Piece movedPiece, final int destinationCoordinate) {
-            super(board, movedPiece, destinationCoordinate);
+        public QueenSideCastleMove(final Board board,
+                                   final Piece movedPiece,
+                                   final int destinationCoordinate,
+                                   final Rook castleRook,
+                                   final int castleRookStart,
+                                   final int castleRookDestination) {
+            super(board, movedPiece, destinationCoordinate, castleRook, castleRookStart, castleRookDestination);
+        }
+
+        @Override
+        public String toString() {
+            return "0-0-0";
         }
     }
 
--- src/com/chess/engine/player/BlackPlayer.java
+++ src/com/chess/engine/player/BlackPlayer.java
@@ -3,9 +3,13 @@ package com.chess.engine.player;
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class BlackPlayer extends Player {
     public BlackPlayer(final Board board, final Collection<Move> whiteStandardLegalMoves, final Collection<Move> blackStandardLegalMoves) {
@@ -25,5 +29,34 @@ public class BlackPlayer extends Player {
     @Override
     public Player getOpponent() {
         return this.board.whitePlayer();
+    }
+
+    @Override
+    protected Collection<Move> calculateKingsCastles(Collection<Move> playerLegals, Collection<Move> opponentsLegals) {
+        final List<Move> kingCastles = new ArrayList<>();
+        if (this.playerKing.isFirstMove() && !this.isCheck()) {
+            //blacks king side castle
+            if (!this.board.getTile(5).isTileOccupied() && !this.board.getTile(6).isTileOccupied()) {
+                final Tile rookTile = this.board.getTile(7);
+                if (rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()) {
+                    if (Player.calculateAttacksOnTile(5, opponentsLegals).isEmpty() &&
+                            Player.calculateAttacksOnTile(6, opponentsLegals).isEmpty() &&
+                            rookTile.getPiece().getPieceType().isRook()) {
+                        //TODO ADD A CASTLEMOVE!
+                        kingCastles.add(null);
+                    }
+                }
+            }
+            if (!this.board.getTile(1).isTileOccupied()
+                    && !this.board.getTile(2).isTileOccupied()
+                    && !this.board.getTile(3).isTileOccupied()) {
+                final Tile rookTile = this.board.getTile(0);
+                if (rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()) {
+                    //TODO add castle move
+                    kingCastles.add(null);
+                }
+            }
+        }
+        return ImmutableList.copyOf(kingCastles);
     }
 }
