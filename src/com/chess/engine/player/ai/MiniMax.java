fe64a8195e3881412d1001bc2a41f7715c54a1ee
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-01-14 10:55:42 +0100

    Done the execute method in the MiniMax class

--- src/com/chess/engine/player/ai/MiniMax.java
+++ src/com/chess/engine/player/ai/MiniMax.java
@@ -18,7 +18,30 @@ public class MiniMax implements MoveStrategy {
 
     @Override
     public Move execute(Board board, int depth) {
-        return null;
+        final long startTime = System.currentTimeMillis();
+        Move bestMove = null;
+        int highestSeenValue = Integer.MIN_VALUE;
+        int lowestSeenValue = Integer.MAX_VALUE;
+        int currentValue;
+        System.out.println(board.currentPlayer() + "THINKING with depth = " + depth);
+        int numMoves = board.currentPlayer().getLegalMoves().size();
+        for (final Move move : board.currentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+            if (moveTransition.getMoveStatus().isDone()) {
+                currentValue = board.currentPlayer().getAlliance().isWhite() ?
+                        min(moveTransition.getTransitionBoard(), depth - 1) :
+                        max(moveTransition.getTransitionBoard(), depth - 1);
+                if (board.currentPlayer().getAlliance().isWhite() && currentValue >= highestSeenValue) {
+                    highestSeenValue = currentValue;
+                    bestMove = move;
+                } else if (board.currentPlayer().getAlliance().isBlack() && currentValue <= lowestSeenValue) {
+                    lowestSeenValue = currentValue;
+                    bestMove = move;
+                }
+            }
+        }
+        final long executionTime = System.currentTimeMillis() - startTime;
+        return bestMove;
     }
 
     public int min(final Board board, final int depth) {
