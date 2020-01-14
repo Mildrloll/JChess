3a296d946a18ea678846410b79d0572af82c310f
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-01-14 10:39:37 +0100

    Started the method on how the MiniMax is gonna work

--- src/com/chess/engine/player/ai/MiniMax.java
+++ src/com/chess/engine/player/ai/MiniMax.java
@@ -2,6 +2,7 @@ package com.chess.engine.player.ai;
 
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.player.MoveTransition;
 
 public class MiniMax implements MoveStrategy {
     private final BoardEvaluator boardEvaluator;
@@ -18,5 +19,39 @@ public class MiniMax implements MoveStrategy {
     @Override
     public Move execute(Board board, int depth) {
         return null;
+    }
+
+    public int min(final Board board, final int depth) {
+        if (depth == 0/*|| game over*/) {
+            return this.boardEvaluator.evaluate(board, depth);
+        }
+        int lowestSeenValue = Integer.MAX_VALUE;
+        for (final Move move : board.currentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+            if (moveTransition.getMoveStatus().isDone()) {
+                final int currentValue = max(moveTransition.getTransitionBoard(), depth - 1);
+                if (currentValue <= lowestSeenValue) {
+                    lowestSeenValue = currentValue;
+                }
+            }
+        }
+        return lowestSeenValue;
+    }
+
+    public int max(final Board board, final int depth) {
+        if (depth == 0/*|| game over*/) {
+            return this.boardEvaluator.evaluate(board, depth);
+        }
+        int highestSeenValue = Integer.MIN_VALUE;
+        for (final Move move : board.currentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+            if (moveTransition.getMoveStatus().isDone()) {
+                final int currentValue = min(moveTransition.getTransitionBoard(), depth - 1);
+                if (currentValue <= highestSeenValue) {
+                    highestSeenValue = currentValue;
+                }
+            }
+        }
+        return highestSeenValue;
     }
 }
