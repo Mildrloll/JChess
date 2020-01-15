7fe1487767378667111161a461ab9e437efa2c52
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-01-15 10:24:26 +0100

    Added more stuff to the StandardBoardEvaluator

--- src/com/chess/engine/player/ai/StandardBoardEvaluator.java
+++ src/com/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -5,6 +5,11 @@ import com.chess.engine.pieces.Piece;
 import com.chess.engine.player.Player;
 
 public final class StandardBoardEvaluator implements BoardEvaluator {
+    private static final int CHECK_BONUS = 50;
+    private static final int CHECK_MATE_BONUS = 10000;
+    private static final int DEPTH_BONUS = 100;
+    private static final int CASTLE_BONUS = 60;
+
     @Override
     public int evaluate(final Board board, final int depth) {
         return scorePlayer(board, board.whitePlayer(), depth -
@@ -12,7 +17,28 @@ public final class StandardBoardEvaluator implements BoardEvaluator {
     }
 
     private int scorePlayer(final Board board, final Player player, final int depth) {
-        return pieceValue(player);
+        return pieceValue(player) + mobility(player) + check(player) +
+                checkmate(player, depth) + castled(player);
+    }
+
+    private static int castled(Player player) {
+        return player.isCastled() ? CASTLE_BONUS : 0;
+    }
+
+    private static int checkmate(Player player, int depth) {
+        return player.getOpponent().isInCheckMate() ? CHECK_MATE_BONUS * depthBonus(depth) : 0;
+    }
+
+    private static int depthBonus(final int depth) {
+        return depth == 0 ? 1 : DEPTH_BONUS * depth;
+    }
+
+    private static int check(final Player player) {
+        return player.getOpponent().isInCheck() ? CHECK_BONUS : 0;
+    }
+
+    private static int mobility(final Player player) {
+        return player.getLegalMoves().size();
     }
 
     private static int pieceValue(final Player player) {
